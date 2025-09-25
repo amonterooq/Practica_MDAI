ce80ea07b2d1778cd0a387fc01f9f40f74b1c541
ana <amonterooq@alumnos.unex.es>
2025-09-25 20:47:19 +0200

    initialize project with Spring Boot and Maven wrapper

---
+++ src/main/java/com/nada/nada/NadaApplication.java
@@ -0,0 +1,13 @@
+package com.nada.nada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NadaApplication.class, args);
+	}
+
+}
